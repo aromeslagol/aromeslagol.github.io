4308521ffea66be9662ff85e3c1e0e5ee6fd1471
澶山稵杞 <163642689+aromeslagol@users.noreply.github.com>
2026-02-15 00:57:14 +0800

    揉揉大家，大家辛苦了！

---
+++ articles/about.js
@@ -0,0 +1,24 @@
+// ===== 博客数据 =====
+window.articlesList = window.articlesList || [];
+window.articlesList.push({
+    id: 401, // 文章ID必须唯一
+    title: '关于作者',
+    category: 'about',
+    date: '2026-02-15',
+    author: 'aromeslagol',
+    readTime: 1,
+    content: `
+        <h2>站长其人</h2>
+        <p>一个普通的幻想者。来自<s>荷兰</s><b>河南</b>的高中<s>住校</s>坐牢生。</p>
+    `
+}, {
+    id: 402,
+    title: '本站使命',
+    category: 'mission',
+    date: '2026-02-15',
+    author: 'aromeslagol',
+    readTime: 1,
+    content: `
+        <p>记录与分享知识、经验与思考，构建一个开放、包容的知识社区，与众多语言与世界幻想者产生共鸣。</p>
+    `
+});
